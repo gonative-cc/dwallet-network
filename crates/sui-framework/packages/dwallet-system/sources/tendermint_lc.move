e03126d8dfefbb6bbe1edfdf5dcec96dede4222a
Vu Vo <vu.voth@gmail.com>
2024-07-31 04:45:17 +0700

    (wip) state proof

--- crates/sui-framework/packages/dwallet-system/sources/tendermint_lc.move
+++ crates/sui-framework/packages/dwallet-system/sources/tendermint_lc.move
@@ -1,4 +1,19 @@
-#[allow(unused_function)]
+#[allow(unused_function, unused_field)]
 module dwallet_system::tendermint_lc {
+    use dwallet::object::{UID};
+
+    struct ConsensusState has key,store {
+        id: UID,
+        timestamp: u64, 
+        next_validators_hash: vector<u8>, 
+        commitment_root: vector<u8>
+    }
+
+    struct ClientState has key, store{
+        id: UID
+    }
+
+    
+    
     public native fun tendermint_state_proof(prefix: vector<u8>, proof: vector<u8>, root: vector<u8>, path: vector<u8>, value: vector<u8>): bool; 
 }
--- sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
+++ sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
@@ -8,6 +8,8 @@ use move_vm_types::{
 use std::collections::VecDeque;
 use smallvec::smallvec;
 
+use ibc::{clients::tendermint::client_state::verify_membership, core::{commitment_types::{commitment::{CommitmentPrefix, CommitmentProofBytes, CommitmentRoot}, proto::ics23::{commitment_proof, CommitmentProof, HostFunctionsManager}, specs::ProofSpecs}, host::types::{identifiers::{ClientId, PortId}, path::{ClientStatePath, CommitmentPath, Path, PortPath}}}};
+
 #[derive(Clone)]
 pub struct TendermintLightClientCostParams {
     pub tendermint_state_proof_cost_base: InternalGas,
@@ -30,7 +32,19 @@ pub fn tendermint_state_proof(
     let root = pop_arg!(args, Vec<u8>);
     let path = pop_arg!(args, Vec<u8>);
     let value = pop_arg!(args, Vec<u8>);
-    Ok(NativeResult::ok(context.gas_used(), smallvec![Value::bool(true)]))
+    
+    let commitment_proof = CommitmentProofBytes::try_from(proof).unwrap();
+    let root = CommitmentRoot::from_bytes(&root);
+    let proof_specs = ProofSpecs::cosmos();
+    let prefix = CommitmentPrefix::try_from(prefix.to_vec()).unwrap();
+
+    let path = Path::Ports(PortPath(PortId::new("10".to_owned()).unwrap()));
+
+    match verify_membership::<HostFunctionsManager>(&proof_specs, &prefix, &commitment_proof, &root, path, value) {
+        Ok(()) => Ok(NativeResult::ok(context.gas_used(), smallvec![Value::bool(true)])),
+        _ =>  Ok(NativeResult::ok(context.gas_used(), smallvec![Value::bool(false)])),
+    }
+
 }
 
 pub fn tendermint_init_lc(
