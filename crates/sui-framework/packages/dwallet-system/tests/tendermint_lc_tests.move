6626a155e23077353037abaabee09abc9a4af9ba
Vu Vo <vu.voth@gmail.com>
2024-08-13 00:59:32 +0700

    add others lc interface

--- crates/sui-framework/packages/dwallet-system/tests/tendermint_lc_tests.move
+++ crates/sui-framework/packages/dwallet-system/tests/tendermint_lc_tests.move
@@ -1,15 +1,11 @@
 #[test_only]
 module dwallet_system::lc_tests {
-    use dwallet_system::tendermint_lc::tendermint_state_proof;
-    // use std::vector;
-
     #[test]
-    fun tendermint_state_proof_test() {
-        let prefix = vector[1, 2, 3];
-        let proof = vector[1, 2, 3];
-        let path = vector[1, 2, 3];
-        let root = vector[1, 2, 3];
-        let value = vector[1, 2, 3];
-        assert!(tendermint_state_proof(prefix, proof, root, path, value) == true, 0);
-    }
+    fun tendermint_state_proof_test() {}
+    #[test]
+    fun tendermint_init_lc_test() {}
+    #[test]
+    fun tendermint_verify_lc_test() {}
+    #[test]
+    fun tendermint_update_lc_test() {}
 }
--- sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
+++ sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
@@ -39,40 +39,7 @@ pub fn tendermint_state_proof(
     ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
-    // TODO: What is ty_args in this case???
-    debug_assert!(ty_args.is_empty());
-    debug_assert!(args.len() == 5);
-
-    let prefix = pop_arg!(args, Vec<u8>);
-    let proof = pop_arg!(args, Vec<u8>);
-    let root = pop_arg!(args, Vec<u8>);
-    let path = pop_arg!(args, Vec<u8>);
-    let value = pop_arg!(args, Vec<u8>);
-
-    let commitment_proof = CommitmentProofBytes::try_from(proof).unwrap();
-    let root = CommitmentRoot::from_bytes(&root);
-    let proof_specs = ProofSpecs::cosmos();
-    let prefix = CommitmentPrefix::try_from(prefix.to_vec()).unwrap();
-
-    let path = Path::Ports(PortPath(PortId::new("10".to_owned()).unwrap()));
-
-    match verify_membership::<HostFunctionsManager>(
-        &proof_specs,
-        &prefix,
-        &commitment_proof,
-        &root,
-        path,
-        value,
-    ) {
-        Ok(()) => Ok(NativeResult::ok(
-            context.gas_used(),
-            smallvec![Value::bool(true)],
-        )),
-        _ => Ok(NativeResult::ok(
-            context.gas_used(),
-            smallvec![Value::bool(false)],
-        )),
-    }
+    todo!()
 }
 
 pub fn tendermint_init_lc(
@@ -80,11 +47,7 @@ pub fn tendermint_init_lc(
     ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
-    // TODO: What is ty_args in this case???
-    Ok(NativeResult::ok(
-        context.gas_used(),
-        smallvec![Value::bool(true)],
-    ))
+    todo!()
 }
 
 pub fn tendermint_verify_lc(
@@ -92,11 +55,7 @@ pub fn tendermint_verify_lc(
     ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
-    // TODO: What is ty_args in this case???
-    Ok(NativeResult::ok(
-        context.gas_used(),
-        smallvec![Value::bool(true)],
-    ))
+    todo!()
 }
 
 pub fn tendermint_update_lc(
@@ -104,9 +63,5 @@ pub fn tendermint_update_lc(
     ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
-    // TODO: What is ty_args in this case???
-    Ok(NativeResult::ok(
-        context.gas_used(),
-        smallvec![Value::bool(true)],
-    ))
+    todo!()
 }
