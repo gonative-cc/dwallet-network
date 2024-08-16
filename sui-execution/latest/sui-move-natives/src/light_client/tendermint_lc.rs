a44fea10688106137baa1f11d21dddd6236ed1f5
Vu Vo <vu.voth@gmail.com>
2024-08-16 22:38:19 +0700

    extract verify lc function

--- sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
+++ sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
@@ -8,25 +8,48 @@ use move_vm_types::{
     values::{Value, Vector, VectorRef},
 };
 
-use smallvec::smallvec;
-use tendermint::{Hash, Time};
-use core::time;
-use std::{collections::VecDeque, io::Read, str::FromStr};
-
 use ibc::{
-    apps::transfer::types::proto, clients::tendermint::{client_state::{verify_header, verify_membership}, types::{proto::v1::Header, ConsensusState, Header as TmHeader}}, core::{
+    apps::transfer::types::proto,
+    clients::tendermint::types::error::{Error as LcError, IntoResult},
+    clients::tendermint::{
+        client_state::{verify_header, verify_membership},
+        types::{proto::v1::Header, ConsensusState, Header as TmHeader},
+    },
+    core::{
+        client::types::error::ClientError,
         commitment_types::{
             commitment::{CommitmentPrefix, CommitmentProofBytes, CommitmentRoot},
             proto::ics23::{commitment_proof, CommitmentProof, HostFunctionsManager},
             specs::ProofSpecs,
         },
         host::types::{
-            identifiers::{ClientId, PortId},
+            identifiers::{ChainId, ClientId, PortId},
             path::{ClientStatePath, CommitmentPath, Path, PortPath},
         },
-    }, primitives::{proto::{Any, Protobuf}, serializers::serialize, Timestamp, ToVec}
+    },
+    primitives::{
+        proto::{Any, Protobuf},
+        serializers::serialize,
+        Timestamp, ToVec,
+    },
+};
+use smallvec::smallvec;
+use std::{
+    collections::VecDeque,
+    error::Error,
+    io::{Chain, Read},
+    str::FromStr,
+    time::Duration,
+};
+use tendermint::crypto::default::Sha256;
+use tendermint::{merkle::MerkleHash, Hash, Time};
+use tendermint_light_client_verifier::{
+    options::Options,
+    types::{TrustThreshold, TrustedBlockState, UntrustedBlockState},
+    ProdVerifier, Verifier,
 };
 
+use tendermint::crypto::Sha256 as Sha256Trait;
 #[derive(Clone)]
 pub struct TendermintLightClientCostParams {
     pub tendermint_state_proof_cost_base: InternalGas,
@@ -66,22 +89,37 @@ pub fn tendermint_verify_lc(
     let type_url = "/ibc.lightclients.tendermint.v1.Header".to_string();
     let any = Any {
         type_url,
-        value: header
-    };  
+        value: header,
+    };
 
     let header = TmHeader::try_from(any).unwrap();
 
     let timestamp = String::from_utf8(timestamp).unwrap();
-    
+
     let cs = ConsensusState {
-        next_validators_hash: Hash::from_bytes(tendermint::hash::Algorithm::Sha256, &next_validators_hash).unwrap(),
+        next_validators_hash: Hash::from_bytes(
+            tendermint::hash::Algorithm::Sha256,
+            &next_validators_hash,
+        )
+        .unwrap(),
         root: CommitmentRoot::from_bytes(&commitment_root),
-        timestamp: Time::from_str(&timestamp).unwrap()
+        timestamp: Time::from_str(&timestamp).unwrap(),
     };
 
-    // verify_header(ctx, header, client_id, chain_id, options, verifier)
-    
-    Ok(NativeResult::ok(context.gas_used(), smallvec![Value::bool(true)])) 
+    let five_year: u64 = 5 * 365 * 24 * 60 * 60;
+    let options = Options {
+        clock_drift: Duration::new(40, 0),
+        trust_threshold: TrustThreshold::ONE_THIRD,
+        trusting_period: Duration::new(five_year, 0),
+    };
+
+    let chain_id = ChainId::new("mychain-3").unwrap();
+    let result =
+        verify_header_lc::<Sha256>(&chain_id, &cs, &header, &options, ProdVerifier::default(), Time::from_str(&timestamp).unwrap());
+    Ok(NativeResult::ok(
+        context.gas_used(),
+        smallvec![Value::bool(true)],
+    ))
 }
 
 pub fn tendermint_update_lc(
@@ -90,4 +128,62 @@ pub fn tendermint_update_lc(
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
     todo!()
+}
+
+// verify tendermint(cometBFT) without implement ExtClientValidationContext.
+pub fn verify_header_lc<H: MerkleHash + Sha256Trait + Default>(
+    chain_id: &ChainId,
+    trusted_consensus_state: &ConsensusState,
+    header: &TmHeader,
+    options: &Options,
+    verifier: impl Verifier,
+    timestamp: Time,
+) -> Result<bool, Box<dyn Error>> {
+    header.validate_basic::<H>()?;
+
+    // TODO: make it more sense
+    // header.verify_chain_id_version_matches_height(chain_id)?;
+    {
+        let trusted_state = {
+            header.check_trusted_next_validator_set::<H>(
+                &trusted_consensus_state.next_validators_hash,
+            )?;
+            TrustedBlockState {
+                chain_id: &chain_id
+                    .as_str()
+                    .try_into()
+                    .map_err(|e| ClientError::Other {
+                        description: format!("failed to parse chain id: {}", e),
+                    })?,
+                header_time: trusted_consensus_state.timestamp(),
+                height: header
+                    .trusted_height
+                    .revision_height()
+                    .try_into()
+                    .map_err(|_| ClientError::ClientSpecific {
+                        description: LcError::InvalidHeaderHeight {
+                            height: header.trusted_height.revision_height(),
+                        }
+                        .to_string(),
+                    })?,
+                next_validators: &header.trusted_next_validator_set,
+                next_validators_hash: trusted_consensus_state.next_validators_hash,
+            }
+        };
+
+        let untrusted_state = UntrustedBlockState {
+            signed_header: &header.signed_header,
+            validators: &header.validator_set,
+            // NB: This will skip the
+            // VerificationPredicates::next_validators_match check for the
+            // untrusted state.
+            next_validators: None,
+        };
+
+        let now =timestamp;
+        verifier
+            .verify_update_header(untrusted_state, trusted_state, options, now)
+            .into_result()?;
+    }
+    Ok(true)
 }
