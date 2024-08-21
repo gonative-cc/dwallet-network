24a5ebae56d4ef6c338c22f7187c3d46eca6fe30
Vu Vo <vu.voth@gmail.com>
2024-08-21 17:16:45 +0700

    state proof native function

--- sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
+++ sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
@@ -5,19 +5,16 @@ use move_vm_types::{
     loaded_data::runtime_types::Type,
     natives::function::NativeResult,
     pop_arg,
-    values::{Value, VectorRef},
+    values::{Value, Vector, VectorRef},
 };
 
 use smallvec::smallvec;
-use std::collections::VecDeque;
+use std::{collections::VecDeque, str::from_utf8};
 
 use ibc::{
-    clients::tendermint::client_state::verify_membership,
     core::{
         commitment_types::{
-            commitment::{CommitmentPrefix, CommitmentProofBytes, CommitmentRoot},
-            proto::ics23::{commitment_proof, CommitmentProof, HostFunctionsManager},
-            specs::ProofSpecs,
+            commitment::{CommitmentPrefix, CommitmentProofBytes, CommitmentRoot}, merkle::{apply_prefix, MerkleProof}, proto::ics23::{commitment_proof, CommitmentProof, HostFunctionsManager}, specs::ProofSpecs
         },
         host::types::{
             identifiers::{ClientId, PortId},
@@ -39,7 +36,36 @@ pub fn tendermint_state_proof(
     ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
-    todo!()
+    let proof = pop_arg!(args, Vector).to_vec_u8()?;
+    let path = pop_arg!(args, Vector).to_vec_u8()?;
+    let prefix = pop_arg!(args, Vector).to_vec_u8()?;
+    let root = pop_arg!(args, Vector).to_vec_u8()?;
+    let value = pop_arg!(args, Vector).to_vec_u8()?;
+
+    let Ok(prefix) = CommitmentPrefix::try_from(prefix) else {
+        return Ok(NativeResult::err(context.gas_used(), 1));
+    };
+
+    let Ok(path_str) = from_utf8(path.as_slice()) else {
+        return Ok(NativeResult::err(context.gas_used(), 1));
+    };
+
+    let merkle_path = apply_prefix(&prefix, vec![path_str.to_owned()]);
+    
+    let Ok(proof_bytes) = CommitmentProofBytes::try_from(proof) else {
+        return Ok(NativeResult::err(context.gas_used(), 1)); 
+    };
+    
+    let Ok(merkle_proof) = MerkleProof::try_from(&proof_bytes) else {
+        return Ok(NativeResult::err(context.gas_used(), 1)); 
+    };
+
+    let root = CommitmentRoot::from_bytes(&root);
+    
+    let verified = merkle_proof.verify_membership::<HostFunctionsManager>(&ProofSpecs::cosmos(), root.into(), merkle_path, value, 0).is_ok();
+
+    Ok(NativeResult::ok(context.gas_used(), smallvec![Value::bool(verified)]))
+
 }
 
 pub fn tendermint_init_lc(
