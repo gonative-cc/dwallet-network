bec426900a1605f007a6a83b3028f8cb4be62749
Vu Vo <vu.voth@gmail.com>
2024-08-28 19:54:01 +0700

    return INVALID error

--- sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
+++ sui-execution/latest/sui-move-natives/src/light_client/tendermint_lc.rs
@@ -5,28 +5,24 @@ use move_vm_types::{
     loaded_data::runtime_types::Type,
     natives::function::NativeResult,
     pop_arg,
-    values::{Value, Vector, Struct},
+    values::{Struct, Value, Vector},
 };
 
 use std::{collections::VecDeque, str::from_utf8};
 
-use ibc::core::
-    commitment_types::{
-        commitment::{CommitmentPrefix, CommitmentProofBytes, CommitmentRoot},
-        merkle::{apply_prefix, MerkleProof},
-        proto::ics23::HostFunctionsManager,
-        specs::ProofSpecs,
-    };
+use ibc::core::commitment_types::{
+    commitment::{CommitmentPrefix, CommitmentProofBytes, CommitmentRoot},
+    merkle::{apply_prefix, MerkleProof},
+    proto::ics23::HostFunctionsManager,
+    specs::ProofSpecs,
+};
 
 use prost::Message;
 
 use ibc::{
     clients::tendermint::types::error::{Error as LcError, IntoResult},
     clients::tendermint::types::{ConsensusState, Header as TmHeader},
-    core::{
-        client::types::error::ClientError,
-        host::types::identifiers::ChainId,
-    },
+    core::{client::types::error::ClientError, host::types::identifiers::ChainId},
     primitives::{proto::Any, ToVec},
 };
 use smallvec::smallvec;
@@ -63,21 +59,21 @@ pub fn tendermint_state_proof(
     let proof = pop_arg!(args, Vector).to_vec_u8()?;
 
     let Ok(prefix) = CommitmentPrefix::try_from(prefix) else {
-        return Ok(NativeResult::err(context.gas_used(), 1));
+        return Ok(NativeResult::err(context.gas_used(), INVALID_INPUT));
     };
 
     let Ok(path_str) = from_utf8(path.as_slice()) else {
-        return Ok(NativeResult::err(context.gas_used(), 1));
+        return Ok(NativeResult::err(context.gas_used(), INVALID_INPUT));
     };
 
     let merkle_path = apply_prefix(&prefix, vec![path_str.to_owned()]);
 
     let Ok(proof_bytes) = CommitmentProofBytes::try_from(proof) else {
-        return Ok(NativeResult::err(context.gas_used(), 1));
+        return Ok(NativeResult::err(context.gas_used(), INVALID_INPUT));
     };
 
     let Ok(merkle_proof) = MerkleProof::try_from(&proof_bytes) else {
-        return Ok(NativeResult::err(context.gas_used(), 1));
+        return Ok(NativeResult::err(context.gas_used(), INVALID_INPUT));
     };
 
     let root = CommitmentRoot::from_bytes(&root);
@@ -97,7 +93,6 @@ pub fn tendermint_state_proof(
         smallvec![Value::bool(verified)],
     ))
 }
-
 
 // TODO: remove trace and add document for this funciton.
 #[instrument(level = "trace", skip_all, err)]
